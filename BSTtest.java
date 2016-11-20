5bdf9d5b2449e5ffac334c760c82fe2d32d9ae9c
Christian Brintnall <christianbrintnall@gmail.com>
2016-11-20 11:16:42 -0800

    Add files via upload
    Binary Search Tree is fully implemented, BSTtest just tests every function in it to see if it works. I'll turn it in as one of our test files.

--- BSTtest.java
+++ BSTtest.java
@@ -18,29 +18,49 @@ public class BSTtest {
     BinarySearchTree.treeInsert(testTree, testNode4);
     BinarySearchTree.treeInsert(testTree, testNode5);
     BinarySearchTree.treeInsert(testTree, testNode6);
-    System.out.println("Insert test complete.");
+    System.out.println("Insert test complete!");
 
     System.out.println("Testing inorderTreeWalk..");
     BinarySearchTree.inorderTreeWalk(testTree.root);
-    System.out.println("inorderTreeWalk complete");
+    System.out.println("inorderTreeWalk complete!");
 
-    System.out.println("Testing min/max nodes");
+    System.out.println("Testing min/max nodes...");
     node min = BinarySearchTree.treeMin(testTree.root);
     System.out.println("Min is: " + min.age);
 
     node max = BinarySearchTree.treeMax(testTree.root);
     System.out.println("Max is: " + max.age);
-    System.out.println("Min/max testing complete");
+    System.out.println("Min/max testing complete!");
 
     System.out.println("Testing search...");
     node searchTest = BinarySearchTree.treeSearch(testTree.root, 63);
     System.out.println("Searched node age: " + searchTest.age);
-    System.out.println("Search test complete.");
+    System.out.println("Search test complete!");
 
-    System.out.println("Testing find successor..");
+    System.out.println("Testing find successor...");
     node succTest = BinarySearchTree.treeSuccessor(testNode2);
     System.out.println("The successor is: " + succTest.age);
-    System.out.println("Successor test complete.");
+    System.out.println("Successor test complete!");
+
+    System.out.println("Testing transplant...");
+    BinarySearchTree.transplant(testTree, testNode1, testNode6);
+    System.out.println("Transplant test complete!");
+
+    System.out.println("Testing treeDelete...");
+    System.out.println("Deleting the root..");
+    BinarySearchTree.treeDelete(testTree, testNode1);
+    System.out.println("Root deleted! New tree:");
+    BinarySearchTree.inorderTreeWalk(testTree.root);
+    System.out.println("Deleting a leaf..");
+    BinarySearchTree.treeDelete(testTree, testNode5);
+    System.out.println("Leaf deleted! New tree: ");
+    BinarySearchTree.inorderTreeWalk(testTree.root);
+    System.out.println("Deleting a parent node...");
+    BinarySearchTree.treeDelete(testTree, testNode2);
+    System.out.println("Parent deleted! New tree: ");
+    BinarySearchTree.inorderTreeWalk(testTree.root);
+
+    System.out.println("All test complete!");
   }
 
 }
--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,3 +1,12 @@
+/*
+
+Names: Christian Brintnall, Emily Wright
+
+Description: A Binary Search Tree object that can perform operations upon the tree.
+             Most of the functions are adapted from our algorithms book to fit out own tree.
+
+*/
+
 public class BinarySearchTree {
 
   //Required variables for a BinarySearchTree
@@ -55,6 +64,7 @@ public class BinarySearchTree {
     return x;
   }
 
+  //Takes in a tree's root, and a key and returns the node with that key (age).
   public static node treeSearch(node x, int key) {
     if ((x == null) || (key == x.age)) {
       return x;
@@ -66,6 +76,7 @@ public class BinarySearchTree {
     }
   }
 
+  //Takes in a node and finds the node containing the successor to that.
   public static node treeSuccessor(node x) {
     if (x.right != null) {
       return treeMin(x.right);
@@ -78,6 +89,37 @@ public class BinarySearchTree {
     return y;
   }
 
-  
+  //Moves nodes u into node z's position
+  public static void transplant(BinarySearchTree tree, node u, node v) {
+    if (u.parent == null) {
+      tree.root = v;
+    } else if (u == u.parent.left) {
+      u.parent.left = v;
+    } else {
+      u.parent.right = v;
+    }
+    if (v != null) {
+      v.parent = u.parent;
+    }
+  }
 
-}
+  //Deletes a node from a tree, uses three cases to determine how this is performed.
+  public static void treeDelete(BinarySearchTree tree, node z) {
+    if (z.left == null) {
+      transplant(tree, z, z.right);
+    } else if (z.right == null) {
+      transplant(tree, z, z.left);
+    } else {
+      node y = treeMin(z.right);
+      if (y.parent != z) {
+        transplant(tree, y, y.right);
+        y.right = z.right;
+        y.right.parent = y;
+      }
+      transplant(tree, z, y);
+      y.left = z.left;
+      y.left.parent = y;
+    }
+  }
+
+} //End BinarySearchTree
