b53780e0734d55d5c77f34fde4dc680efa09a56c
Christian Brintnall <christianbrintnall@gmail.com>
2016-11-20 16:46:29 -0800

    The main driver file (finished)

--- names.java
+++ names.java
@@ -3,49 +3,67 @@
 Names: Christian Brintnall, Emily Wright
 Description: This is the driver file for the three data structures/functions.
              A user will input their chosen data structure and this program will user
-             that data structure to display data.
+             that data structure to display data. All the handle functions are contained
+             in handle.java.
+
+Issues: The scanner needs to be reset, otherwise you can only pick one data structure and it won't be able to load data with the others.
+        This can be fixed multiple ways, by loading the data structures before we do anything (this would take A LONG time) or by
+        creating a scanner when the option is chosen, I'm going to talk to Moushomi about it Monday.
+
+        Just kidding I fixed it by putting it in the loop too.
 
 */
+
 import java.util.Scanner;
 import java.io.*;
 
 public class names {
 
+
+  //Our main function, where we see what data structure is used.
   public static void main (String[] args) {
 
     try {
 
-      File yearFile = new File("yob2014.txt");
-      Scanner yearScan = new Scanner(yearFile);
+
+      while (true) {
+
+        System.out.print("\nPlease input a text file: ");
+        Scanner uInput = new Scanner(System.in);
+        String userInput = uInput.next();
+        File yearFile = new File(userInput);
+        Scanner yearScan = new Scanner(yearFile);
+
+        System.out.println("\nWhich data structure would you like to use? 1: Tree, 2: HashMap, 3: ArrayList");
+        Scanner userScan = new Scanner(System.in);
+        int userChoice = userScan.nextInt();
+
+        if (userChoice == 1) {
+
+          //This functions does everything the tree should.
+          handle.handleTree(yearScan);
+
+        } else if (userChoice == 2) {
+
+          //This function does everything the HashMap should
+          handle.handleHashMap(yearScan);
+
+        } else if (userChoice == 3) {
+
+          //This function does everything the ArrayList should
+          handle.handleArrayList(yearScan);
+
+        } else {
+
+          System.out.println("Not a valid choice, please enter a number between 1-3.");
+        }
+      }
+
     } catch (FileNotFoundException e) {
 
-      System.out.println("Please ensure yob2014.txt is in the current directory and try again.");
+      System.out.println("Please input a valid YOB text file, with the format [name], [gender], [frequency].");
       System.exit(0);
-    }
 
-    while (true) {
-
-      System.out.println("\nWhich data structure would you like to use? 1: Tree, 2: HashMap, 3: ArrayList");
-      Scanner userScan = new Scanner(System.in);
-      int userChoice = userScan.nextInt();
-      if (userChoice == 1) {
-
-        System.out.println("You chose: Tree");
-        System.out.println("Loading the data structure, due to large file size this may take a second. There will be a message indicating when complete");
-      } else if (userChoice == 2) {
-
-        System.out.println("You chose: HashMap");
-        System.out.println("Loading the data structure, due to large file size this may take a second. There will be a message indicating when complete"); // Emily please make sure to add this after it's done loading.
-        System.out.println("Emily this is where you load the HashMap with necessary info");
-      } else if (userChoice == 3) {
-
-        System.out.println("You chose: ArrayList");
-        System.out.println("Loading the data structure, due to large file size this may take a second. There will be a message indicating when complete"); // Emily please make sure to add this after it's done loading.
-        System.out.println("Emily this is where you load the ArrayList with necessary info");
-      } else {
-
-        System.out.println("Not a valid choice, please enter a number between 1-3.");
-      }
     }
   }
 }
