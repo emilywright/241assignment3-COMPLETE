fb7c20631db56d5b592001321dc9866e66b9df07
Emily Wright <emily.wright005@gmail.com>
2016-11-21 20:36:14 -0800

    Add files via upload

---
+++ ArrayListFile.java
@@ -0,0 +1,18 @@
+/*  Function descriptions:
+   -SearchName for a name returns number of male and female babies born in that year who has that name.
+    It should also return a rank for this name (how popular is this name for boys and girls).
+
+   -MostPopularName returns the most popular 10 male and female baby names for a
+    given year with their numbers and percentage of babies with that name.
+
+   -UniqueName returns 5 male and female baby names that are unique with their frequency and percentage of babies with that name.
+    For this method, ignore names that appear less than 5 times.
+
+   -DisplayName prints the names in alphabetical order,
+    and next to each name prints the number of male and female babies that have this
+    name and percentage of babies (male and female) for that name.
+*/
+
+public class ArrayListFile {
+
+}
